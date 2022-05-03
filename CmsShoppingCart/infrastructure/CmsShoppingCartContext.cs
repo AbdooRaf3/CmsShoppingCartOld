72e896379799078bb076c846e2bf240e20a5785a
User <User@DESKTOP-V6SOIC9>
2022-05-04 00:59:02 +0300

    Add project files.

---
+++ CmsShoppingCart/infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.infrastructure
+{
+    public class CmsShoppingCartContext :DbContext
+     
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            : base(options) 
+        { 
+        }
+    }
+}
